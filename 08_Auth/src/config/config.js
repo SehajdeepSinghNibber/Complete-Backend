7160b551954cc70e5e4eefb977f7b9b829e3cb7b
Sehajdeep Singh <ssingh28_be24@thapar.edu>
2026-04-02 00:58:00 +0530

    registering a user is done

--- 08_Auth/src/config/config.js
+++ 08_Auth/src/config/config.js
@@ -3,11 +3,16 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 if (!process.env.MONGO_URI){
-    throw new Error("Mongo_URI is not definned in environment variables")
+    throw new Error("Mongo_URI is not defined in environment variables")
 }
 
 const config = {
-    MONGO_URI:process.env.MONGO_URI
+    MONGO_URI:process.env.MONGO_URI,
+    JWT_SECRET:process.env.JWT_SECRET
+}
+
+if(!process.env.JWT_SECRET){
+    throw new Error("JWT_SECRET is not defined in environment variable")
 }
 
 export default config
---
+++ 08_Auth/src/controllers/auth.controller.js
@@ -0,0 +1,48 @@
+import userModel from '../models/user.model.js'
+import crypto from 'crypto'
+import jwt from 'jsonwebtoken'
+import config from"../config/config.js"
+
+export const register = async (req,res)=>{
+
+    const { username, email, password } = req.body;
+
+    const isAlreadyRegistered = await userModel.findOne({
+        $or:[
+            { username },
+            { email }
+        ]
+    })
+
+    if(isAlreadyRegistered){
+        res.status(409).json({
+            message: "Username or email already exists!!"
+        })
+    }
+
+    const hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    const token = jwt.sign({
+        id: user._id
+    },config.JWT_SECRET,
+    {
+        expiresIn: "1d"
+        //1h for 1 hour, 1d for 1 day
+    })
+
+    res.status(201).json({
+        message:"User Registered Successfully",
+    user:{
+        username: user.username,
+        email: user.email
+    },
+    token
+    })
+
+}   
